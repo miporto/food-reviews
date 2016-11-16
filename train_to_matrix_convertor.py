fc028db52c0c858af94ecc0524bac831f4927a68
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-11-16 16:08:52 -0300

    Add test support

--- train_to_matrix_convertor.py
+++ train_to_matrix_convertor.py
@@ -1,30 +1,36 @@
 import csv
 import uuid
-import hashlib
 
-train_to_read = 'train.csv'
-prime_number = 127
-salt = uuid.uuid4().hex
+def word2matrix(train_to_read, output, test=False):
+	prime_number = 127
+	salt = uuid.uuid4().hex
 
-with open(train_to_read) as csvReadfile:
-    with open('matrix_of_word_frequency.csv', 'w') as csvWritefile:
-        fieldnames = ['ProductId', 'Prediction']
-        writer = csv.DictWriter(csvWritefile, fieldnames=fieldnames)
+	with open(train_to_read) as csvReadfile:
+		with open(output, 'w') as csvWritefile:
+			if (test):
+				fieldnames = ['Id']	
+			else:
+				fieldnames = ['Id', 'Prediction']
+			writer = csv.DictWriter(csvWritefile, fieldnames=fieldnames)
 
-        writer.writeheader()
-        reader = csv.DictReader(csvReadfile)
+			writer.writeheader()
+			reader = csv.DictReader(csvReadfile)
 
-        for row in reader:
-            hash_array = [0] * prime_number
+			for row in reader:
+				hash_array = [0] * prime_number
 
-            text = row['Summary'] + row['Text']
-            for word in text.split():
-                hashed_number = (int(hashlib.sha256(salt.encode() + word.encode()).hexdigest(), 16) % prime_number)
-                hash_array[hashed_number] += 1
+				text = row['Summary'] + row['Text']
+				for word in text.split():
+					hashed_number = int(hash(word.encode()) % prime_number)
+					hash_array[hashed_number] += 1
 
-            csvWritefile.write('%s,' % row['ProductId'].split()[0])
-            csvWritefile.write('%s' % row['Prediction'].split()[0])
+				csvWritefile.write('%s' % row['Id'].split()[0])
+				
+				if (not test):
+					csvWritefile.write(',%s' % row['Prediction'].split()[0])
 
-            for i in range(0, prime_number):
-                csvWritefile.write(',%s' % str(hash_array[i]))
-            csvWritefile.write('\n')
+				for i in range(0, prime_number):
+					csvWritefile.write(',%s' % str(hash_array[i]))
+				csvWritefile.write('\n')
+
+word2matrix("data/sparktest.csv", "data/matrixtest.csv", test=True)
