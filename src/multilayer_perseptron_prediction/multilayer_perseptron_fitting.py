c124c8bc4d1f4839fba46369039ef6c14c9330a3
SantiagoLazzari <santiago.lazzari@mercadolibre.com>
2016-11-18 12:52:36 -0300

    Multilayer perseptron prediction

---
+++ src/multilayer_perseptron_prediction/multilayer_perseptron_fitting.py
@@ -0,0 +1,61 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.externals import joblib
+
+import csv
+import random as rd
+
+# Matrix of registers
+registers = []
+
+# Array of predictions in the same order that the matrix of registers
+predictions = []
+
+# Open the file with the registers
+with open("../../data/matrix_of_word_frequency.csv", "r") as csvfile:
+    firstValue = True
+    for row in csv.reader(csvfile):
+        if firstValue:
+            firstValue = False
+            continue
+
+        register = [float(x) for x in row[2::]]
+        prediction = float(row[1])
+
+        registers.append(register)
+        predictions.append(prediction)
+
+
+
+
+activations = ['identity', 'logistic', 'tanh']
+solvers = ['lbfgs', 'sgd', 'adam']
+learning_rates = ['constant', 'invscaling', 'adaptive']
+
+
+for i in xrange(100, 1000):
+    activation = activations[rd.randrange(len(activations))]
+    solver = solvers[rd.randrange(len(solvers))]
+    learning_rate = learning_rates[rd.randrange(len(learning_rates))]
+
+    layers = tuple([rd.randrange(10, 100) for x in xrange(rd.randrange(1, 10))])
+    alpha = 1 / float(rd.randrange(1, 100000))
+    momentum = 900 / float(rd.randrange(901, 1400))
+    beta_1 = 900 / float(rd.randrange(901, 1400))
+    beta_2 = 900 / float(rd.randrange(901, 1000)) #Closer to 0.999
+
+    print "Start training net", i, "params : "
+    print "activation :", activation
+    print "solver :", solver
+    print "learning_rate :", learning_rate
+    print "layers :", layers
+    print "alpha :", alpha
+    print "momentum :", momentum
+    print "beta_1 :", beta_1
+    print "beta_2 :", beta_2
+
+    clf = MLPClassifier(activation=activation, solver=solver, alpha=alpha, hidden_layer_sizes=layers, \
+    learning_rate=learning_rate, momentum=momentum, beta_1=beta_1, beta_2=beta_2,\
+    random_state=1, verbose=True)
+
+    clf.fit(registers, predictions)
+    joblib.dump(clf, 'newral_networks/newral_network_max_iterations_' + str(i) + '.pkl')
---
+++ src/multilayer_perseptron_prediction/multilayer_perseptron_prediction.py
@@ -0,0 +1,59 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.externals import joblib
+
+import csv
+import random as rd
+
+def makeKaggleSubmition():
+    clf = joblib.load('newral_network_max_iterations_40.pkl')
+
+    # Open the file with the registers
+    with open("../../data/matrix_of_word_frequency_test2.csv", "r") as csvfile, open("../../data/kaggle.csv", "w") as kaggle:
+        firstValue = True
+        for row in csv.reader(csvfile):
+            if firstValue:
+                firstValue = False
+                kaggle.write("Id,Prediction\n")
+                continue
+
+            registerId = row[0]
+
+            register = [[float(x) for x in row[1::]]]
+            prediction = int(clf.predict(register))
+
+            kaggle.write("{},{}\n".format(registerId, prediction))
+
+def crossValidateWithTest():
+    for x in range(29):
+        clf = joblib.load('newral_networks/newral_network_max_iterations_' + str(x) + '.pkl')
+
+        correctPredictions = 0
+        totalPredictions = 0
+        # Open the file with the register
+        with open("../../data/matrix_of_word_frequency.csv", "r") as csvfile:
+            firstValue = True
+
+            for row in csv.reader(csvfile):
+                if rd.randrange(10) != 5:
+                    continue
+                if firstValue:
+                    firstValue = False
+                    continue
+
+                registerId = row[0]
+                correctPrediction = int(row[1])
+                register = [[float(x) for x in row[2::]]]
+                prediction = int(clf.predict(register))
+
+                correctPredictions += correctPrediction == prediction
+                totalPredictions += 1
+
+        print "Net ID " + str(x) + "Correct prediction", correctPredictions, "total predictions", totalPredictions, "ratio", float(correctPredictions)/totalPredictions
+        print clf
+
+def main():
+    crossValidateWithTest()
+    return 0
+
+if __name__ == "__main__":
+    main()
