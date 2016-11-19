0778968737c3f8602ef13d7e541463d38f0818f9
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-11-19 08:06:58 -0300

    Command line arguments

---
+++ word2matrix.py
@@ -0,0 +1,50 @@
+# WORD2MATRIX
+# test example: python word2matrix.py testinput.csv testoutput.csv test
+# train example: python word2matrix.py traininput.csv trainoutput.csv train
+
+import sys
+import csv
+
+import uuid
+
+def hashing_trick_summarytext(train_to_read, output, test=False):
+	prime_number = 127
+	salt = uuid.uuid4().hex
+
+	with open(train_to_read) as csvReadfile:
+		with open(output, 'w') as csvWritefile:
+			if (test):
+				fieldnames = ['Id']	
+			else:
+				fieldnames = ['Id', 'Prediction']
+			writer = csv.DictWriter(csvWritefile, fieldnames=fieldnames)
+
+			writer.writeheader()
+			reader = csv.DictReader(csvReadfile)
+
+			for row in reader:
+				hash_array = [0] * prime_number
+
+				text = row['Summary'] + row['Text']
+				for word in text.split():
+					hashed_number = int(hash(word.encode()) % prime_number)
+					hash_array[hashed_number] += 1
+
+				csvWritefile.write('%s' % row['Id'].split()[0])
+				
+				if (not test):
+					csvWritefile.write(',%s' % row['Prediction'].split()[0])
+
+				for i in range(0, prime_number):
+					csvWritefile.write(',%s' % str(hash_array[i]))
+				csvWritefile.write('\n')
+
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+if sys.argv[3] == "test":
+	test = True
+else:
+	test = False
+
+hashing_trick_summarytext(input_file, output_file, test=test)
